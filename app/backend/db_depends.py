96628c0e73acdaf0479c8daf91da2c086c1f8faa
RevelRies <ayzikov070@mail.ru>
2025-05-19 22:35:41 +0300

    refactor: refactor JWT authorization system  - remove ability to set a different expires_delta when creating tokens  - make authorization dependency async  - made a separate file with dependencies and moved them all into it

--- app/backend/db_depends.py
+++
@@ -1,9 +0,0 @@
-# install
-from sqlalchemy.ext.asyncio import AsyncSession
-# local
-from app.backend.db import async_session_maker
-
-
-async def get_db_session() -> AsyncSession:
-    async with async_session_maker() as session:
-        yield session
